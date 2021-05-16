9a42b5f02e80354137141a9e5f61341c34e13648
Del <Delkdl04@gmail.com>
2021-05-16 16:35:25 +1200

    Initial sharing of project

---
+++ FlagStaff.java
@@ -0,0 +1,29 @@
+
+/**
+ * Creates a FlagStaff object
+ * raises and lowers a flag on a pole.
+ *
+ * @author Del Huang
+ * @version 16/05/21k
+ */
+public class FlagStaff
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class FlagStaff
+     */
+    public FlagStaff()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    public void doFlagStaff() {
+    
+        Flag flag = new Flag();
+        
+    }
+    
+}
